eef3ae2bdf9b8aa0bb89a2b24f87acd762097c5e
Frank Klingebiel <klingebiel@optimal-systems.de>
2016-04-28 22:47:18 +0200

    First commit

---
+++ express-selfapi.js
@@ -0,0 +1,111 @@
+var registry = []
+
+
+
+exports.registry = registry;
+exports.use = function(app,def) {
+  registerDefinition(app,{
+    _registry : {
+      description : "Registry self describing service",
+      endpoints : {
+          get : {
+            path : '',
+            use : function getRegistry(req,res) {
+             res.send( registry );
+            }
+          }
+        }
+    }
+  });
+
+  registerDefinition(app,def);
+}
+
+function registerDefinition(app,def) {
+  for( var service in def ) {
+
+    if( !def[service].endpoints ) {
+      throw new Error('No use endpoints defined in service '+service);
+    }
+
+    var servicedefinition = {
+      key : service,
+      path : def[service].path,
+      description : def[service].description,
+      endpoints : []
+    }
+
+    if( !servicedefinition.path ) servicedefinition.path = service;
+
+    for( var endpoint in def[service].endpoints ) {
+      servicedefinition.endpoints.push( registerEndpoint(app, {
+          service     : servicedefinition,
+          service_raw : def[service],
+          endpointkey : endpoint,
+          endpoint    : def[service].endpoints[endpoint]
+      } ) );
+    }
+
+    registry.push( servicedefinition );
+
+  }
+}
+
+function registerEndpoint(app,def) {
+
+  if( !def.endpoint.use ) {
+    throw new Error('No use function defined in endpoint '+def.endpointkey);
+  }
+  var endpointDefinition = {
+    method : def.endpoint.method || 'GET',
+    description : def.endpoint.description,
+    path : def.endpoint.path || def.endpointkey,
+    parameter : []
+  };
+
+  // Determine path
+  endpointDefinition.fullpath = '/'+ def.service.path;
+  if( def.endpoint.path !== undefined ) {
+    if( def.endpoint.path.length>0 ) {
+      endpointDefinition.path = def.endpoint.path;
+      endpointDefinition.fullpath += '/'+ endpointDefinition.path;
+    }
+  } else {
+    endpointDefinition.path = def.endpointkey;
+    endpointDefinition.fullpath += '/'+ endpointDefinition.path;
+  }
+
+  // Determine parameter
+  var allparams = collect(def.service_raw.parameter, def.endpoint.parameter);
+  for( var param in allparams ) {
+    endpointDefinition.parameter.push({
+      name        : param,
+      type        : allparams[param].type || "STRING",
+      description : allparams[param].description,
+      path        : allparams[param].path || false
+    });
+  }
+  console.log('registered endpoint : '+endpointDefinition.method+' '+endpointDefinition.fullpath+' uses '+def.endpoint.use.name);
+
+  app.get( endpointDefinition.fullpath, function(req, res)  {
+    req.endpoint = endpointDefinition;
+    def.endpoint.use(req, res);
+  });
+
+  return endpointDefinition;
+
+};
+
+
+function collect() {
+  var ret = {};
+  var len = arguments.length;
+  for (var i=0; i<len; i++) {
+    for (var p in arguments[i]) {
+      if (arguments[i].hasOwnProperty(p)) {
+        ret[p] = arguments[i][p];
+      }
+    }
+  }
+  return ret;
+}
---
+++ index.js
@@ -0,0 +1,66 @@
+
+var rgb_query_parameter = {
+  r : { type : "INT", default: 0, description : "Red color part"},
+  g : { type : "INT", default: 0, description : "Green color part"},
+  b : { type : "INT", default: 0, description : "Blue color part"}
+};
+
+var def = {
+  blink : {
+    path: "blink/:id",
+    description : "Let the LEDs blink service.",
+    parameter : {
+      id : { type : "INT", description : "LED Identifier", path:true }
+    },
+    endpoints : {
+      off : {
+        description : "Switch LED off.",
+        use : echo
+      },
+      script: {
+        path : "script/:scriptid",
+        description : "Executes a pre-defined script",
+        parameter : {
+          scriptid : { type : "INT", description : "Script ID", path : true }
+        },
+        use : echo
+      },
+      rgb: {
+        description : "Switch to rgb color",
+        parameter : rgb_query_parameter,
+        use : echo
+      },
+      fade: {
+        description : "Fade to rgb color",
+        parameter : rgb_query_parameter,
+        use : echo
+      }
+    }
+  }
+};
+
+function echo( req, res ) {
+  res.send( req.endpoint );
+}
+
+var express     = require('express');
+var app = express();
+var api = require('./express-selfapi.js');
+
+api.use(app, def);
+
+app.listen(3000, function () {
+  console.log('Self api app listening on port 3000!');
+});
+
+
+/*function show(s) {
+  if( s ) {
+    for( var p in s ) {
+      console.log('propery : '+p+" "+s[p] );
+    }
+  } else {
+      console.log('--undefined');
+  }
+}
+*/
